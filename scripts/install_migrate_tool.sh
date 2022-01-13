0e5efe743c2a79d959e98b51947d55477b8957bc
zhoujian2 <zhoujian2@sensetime.com>
2022-01-13 23:19:53 +0800

    use migrate tool

---
+++ scripts/install_migrate_tool.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    curl https://github.com/golang-migrate/migrate/releases/download/v4.15.1/migrate.darwin-amd64.tar.gz -O -L
+    mkdir migrate-tmp
+    tar -C ./migrate-tmp -zxvf migrate.darwin-amd64.tar.gz
+    sudo mv ./migrate-tmp/migrate /usr/local/bin
+    rm migrate.darwin-amd64.tar.gz
+    rm -rf migrate-tmp
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    curl https://github.com/golang-migrate/migrate/releases/download/v4.15.1/migrate.linux-amd64.tar.gz -O -L
+    mkdir migrate-tmp
+    tar -C ./migrate-tmp -zxvf migrate.linux-amd64.tar.gz
+    sudo mv ./migrate-tmp/migrate /usr/local/bin
+    rm migrate.linux-amd64.tar.gz
+    rm -rf migrate-tmp
+fi
