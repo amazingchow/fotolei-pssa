d2ce9a354d86b8fac83ba533f970bf3ca2fcb018
zhoujian2 <zhoujian2@sensetime.com>
2022-04-02 20:37:06 +0800

    move dockerfiles to mysql_dockerfile and move docker-compose files to mysql_docker_compose

--- scripts/init_env.sh
+++ scripts/init_env.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-mkdir -p ~/fotolei-pssa-db/data
-mkdir -p ~/fotolei-pssa-db/logs
-mkdir -p ~/fotolei-pssa-db/conf.d
-cp $PWD/test/mysql-deploy/conf.d/*.cnf ~/fotolei-pssa-db/conf.d
-mkdir -p ~/fotolei-pssa-db/migrations
-cp $PWD/db/migrations/*.sql ~/fotolei-pssa-db/migrations
-mkdir -p ~/fotolei-pssa/logs
-mkdir -p ~/fotolei-pssa/tmp-files
-mkdir -p ~/fotolei-pssa/inventories
-mkdir -p ~/fotolei-pssa/jit-inventory
-mkdir -p ~/fotolei-pssa/products
-mkdir -p ~/fotolei-pssa/send_queue
-mkdir -p ~/fotolei-pssa/session
-mkdir -p ~/fotolei-pssa/recv_queue
-mkdir -p ~/fotolei-pssa/unit-price
+mkdir -p $HOME/fotolei-pssa-db/data
+mkdir -p $HOME/fotolei-pssa-db/logs
+mkdir -p $HOME/fotolei-pssa-db/docker-compose
+cp $PWD/mysql_docker_compose/docker-compose.yml $HOME/fotolei-pssa-db/docker-compose
+mkdir -p $HOME/fotolei-pssa-db/conf.d
+cp $PWD/mysql_docker_compose/conf.d/*.cnf $HOME/fotolei-pssa-db/conf.d
+mkdir -p $HOME/fotolei-pssa-db/migrations
+cp $PWD/db/migrations/*.sql $HOME/fotolei-pssa-db/migrations
+mkdir -p $HOME/fotolei-pssa/logs
+mkdir -p $HOME/fotolei-pssa/tmp-files
+mkdir -p $HOME/fotolei-pssa/inventories
+mkdir -p $HOME/fotolei-pssa/jit-inventory
+mkdir -p $HOME/fotolei-pssa/products
+mkdir -p $HOME/fotolei-pssa/send_queue
+mkdir -p $HOME/fotolei-pssa/session
+mkdir -p $HOME/fotolei-pssa/recv_queue
+mkdir -p $HOME/fotolei-pssa/unit-price
 
-docker-compose -f "$PWD/test/mysql-deploy/docker-compose.yml" up -d --build
-docker_container_id=`docker container ls | grep mysql-deploy | awk '{print $1}'`
+docker-compose -f "$HOME/fotolei-pssa-db/docker-compose/docker-compose.yml" up -d --build
+docker_container_id=`docker container ls | grep docker-compose_db_1 | awk '{print $1}'`
 sleep 2
 docker exec -i ${docker_container_id} /bin/bash -c 'mysql -uroot -pPwd123Pwd < /mysql/migrations/create_database_up.sql'
 
