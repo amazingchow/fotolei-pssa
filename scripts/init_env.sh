6c40751b5c33f6d5aeef5fdd99762f8246cf03b9
zhoujian2 <zhoujian2@sensetime.com>
2022-04-04 10:21:53 +0800

    move migrations dir out of db

--- scripts/init_env.sh
+++ scripts/init_env.sh
@@ -7,7 +7,7 @@ cp $PWD/mysql_docker_compose/docker-compose.yml $HOME/fotolei-pssa-db/docker-com
 mkdir -p $HOME/fotolei-pssa-db/conf.d
 cp $PWD/mysql_docker_compose/conf.d/*.cnf $HOME/fotolei-pssa-db/conf.d
 mkdir -p $HOME/fotolei-pssa-db/migrations
-cp $PWD/db/migrations/*.sql $HOME/fotolei-pssa-db/migrations
+cp $PWD/migrations/*.sql $HOME/fotolei-pssa-db/migrations
 mkdir -p $HOME/fotolei-pssa/logs
 mkdir -p $HOME/fotolei-pssa/tmp-files
 mkdir -p $HOME/fotolei-pssa/inventories
@@ -23,7 +23,7 @@ docker_container_id=`docker container ls | grep docker-compose_db_1 | awk '{prin
 sleep 2
 docker exec -i ${docker_container_id} /bin/bash -c 'mysql -uroot -pPwd123Pwd < /mysql/migrations/create_database_up.sql'
 
-migrate -source file://$PWD/db/migrations -database "mysql://root:Pwd123Pwd@(127.0.0.1:13306)/fotolei_pssa" up
+migrate -source file://$PWD/migrations -database "mysql://root:Pwd123Pwd@(127.0.0.1:13306)/fotolei_pssa" up
 if [ $? -ne 0 ]; then
-    docker run --rm -v $PWD/db/migrations:/migrations --network host migrate/migrate:v4.15.1 -path=/migrations/ -database "mysql://root:Pwd123Pwd@(127.0.0.1:13306)/fotolei_pssa" up
+    docker run --rm -v $PWD/migrations:/migrations --network host migrate/migrate:v4.15.1 -path=/migrations/ -database "mysql://root:Pwd123Pwd@(127.0.0.1:13306)/fotolei_pssa" up
 fi
