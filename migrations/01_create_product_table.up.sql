e670649ff40d98374a689791aeade02243e7f95e
zhoujian2 <zhoujian2@sensetime.com>
2022-05-04 16:34:42 +0800

    [sql-fix] add default value for product table and inventory table

--- migrations/01_create_product_table.up.sql
+++ migrations/01_create_product_table.up.sql
@@ -1,26 +1,26 @@
 CREATE TABLE IF NOT EXISTS fotolei_pssa.products (
     id                 INT           NOT NULL AUTO_INCREMENT,
-    product_code       VARCHAR(64),              /* 商品编码 */
-    product_name       VARCHAR(128),             /* 商品名称 */
-    specification_code VARCHAR(64)   NOT NULL,   /* 规格编码 */
-    specification_name VARCHAR(128),             /* 规格名称 */
-    brand              VARCHAR(64),              /* 品牌 */
-    classification_1   VARCHAR(64),              /* 分类1 */
-    classification_2   VARCHAR(64),              /* 分类2 */
-    product_series     VARCHAR(64),              /* 产品系列 */
-    stop_status        VARCHAR(32),              /* STOP状态 */
-    product_weight     FLOAT,        DEFAULT 0.0 /* 重量/g */
-    product_length     FLOAT,        DEFAULT 0.0 /* 长度/cm */
-    product_width      FLOAT,        DEFAULT 0.0 /* 宽度/cm */
-    product_height     FLOAT,        DEFAULT 0.0 /* 高度/cm */
-    is_combined        VARCHAR(32),              /* 是否是组合商品 */
-    be_aggregated      VARCHAR(32),              /* 是否参与统计 */
-    is_import          VARCHAR(32),              /* 是否是进口商品 */
-    supplier_name      VARCHAR(128),             /* 供应商名称 */
-    purchase_name      VARCHAR(128),             /* 采购名称 */
-    jit_inventory      INT,          DEFAULT 0   /* 实时可用库存 */
-    moq                INT,          DEFAULT 0   /* 最小订货单元 */
-    unit_price         FLOAT,        DEFAULT 0.0 /* 单价 */
+    product_code       VARCHAR(64),                           /* 商品编码 */
+    product_name       VARCHAR(128),                          /* 商品名称 */
+    specification_code VARCHAR(64)   NOT NULL,                /* 规格编码 */
+    specification_name VARCHAR(128),                          /* 规格名称 */
+    brand              VARCHAR(64),                           /* 品牌 */
+    classification_1   VARCHAR(64),                           /* 分类1 */
+    classification_2   VARCHAR(64),                           /* 分类2 */
+    product_series     VARCHAR(64),                           /* 产品系列 */
+    stop_status        VARCHAR(32),                           /* STOP状态 */
+    product_weight     FLOAT(10,2)   NOT NULL DEFAULT '0.00', /* 重量/g */
+    product_length     FLOAT(10,2)   NOT NULL DEFAULT '0.00', /* 长度/cm */
+    product_width      FLOAT(10,2)   NOT NULL DEFAULT '0.00', /* 宽度/cm */
+    product_height     FLOAT(10,2)   NOT NULL DEFAULT '0.00', /* 高度/cm */
+    is_combined        VARCHAR(32),                           /* 是否是组合商品 */
+    be_aggregated      VARCHAR(32),                           /* 是否参与统计 */
+    is_import          VARCHAR(32),                           /* 是否是进口商品 */
+    supplier_name      VARCHAR(128),                          /* 供应商名称 */
+    purchase_name      VARCHAR(128),                          /* 采购名称 */
+    jit_inventory      INT           NOT NULL DEFAULT 0,      /* 实时可用库存 */
+    moq                INT           NOT NULL DEFAULT 0,      /* 最小订货单元 */
+    unit_price         FLOAT(10,2)   NOT NULL DEFAULT '0.00', /* 单价 */
     PRIMARY KEY (id),
     KEY products_specification_code (specification_code),
     KEY products_specification_code_jit_inventory (specification_code, jit_inventory),
--- migrations/03_create_inventory_table.up.sql
+++ migrations/03_create_inventory_table.up.sql
@@ -1,29 +1,29 @@
 CREATE TABLE IF NOT EXISTS fotolei_pssa.inventories (
     id                         INT           NOT NULL AUTO_INCREMENT,
-    product_code               VARCHAR(64),              /* 商品编码 */
-    product_name               VARCHAR(128),             /* 商品名称 */
-    specification_code         VARCHAR(64)   NOT NULL,   /* 规格编码 */
-    specification_name         VARCHAR(128),             /* 规格名称 */
-    st_inventory_qty           INT,          DEFAULT 0   /* 起始库存数量 */
-    st_inventory_total         FLOAT,        DEFAULT 0.0 /* 起始库存总额 */
-    purchase_qty               INT,          DEFAULT 0   /* 采购数量 */
-    purchase_total             FLOAT,        DEFAULT 0.0 /* 采购总额 */
-    purchase_then_return_qty   INT,          DEFAULT 0   /* 采购退货数量 */
-    purchase_then_return_total FLOAT,        DEFAULT 0.0 /* 采购退货总额 */
-    sale_qty                   INT,          DEFAULT 0   /* 销售数量 */
-    sale_total                 FLOAT,        DEFAULT 0.0 /* 销售总额 */
-    sale_then_return_qty       INT,          DEFAULT 0   /* 销售退货数量 */
-    sale_then_return_total     FLOAT,        DEFAULT 0.0 /* 销售退货总额 */
-    others_qty                 INT,          DEFAULT 0   /* 其他变更数量 */
-    others_total               FLOAT,        DEFAULT 0.0 /* 其他变更总额 */
-    ed_inventory_qty           INT,          DEFAULT 0   /* 截止库存数量 */
-    ed_inventory_total         FLOAT,        DEFAULT 0.0 /* 截止库存总额 */
-    create_time                VARCHAR(10),              /* 年月的格式 */
-    extra_brand                VARCHAR(64),              /* 品牌 */
-    extra_classification_1     VARCHAR(64),              /* 分类1 */
-    extra_classification_2     VARCHAR(64),              /* 分类2 */
-    extra_is_combined          VARCHAR(32),              /* 是否是组合商品 */
-    anchor                     TINYINT,      DEFAULT 0   /* 锚，防止‘组合商品‘读出来带空格 */
+    product_code               VARCHAR(64),                           /* 商品编码 */
+    product_name               VARCHAR(128),                          /* 商品名称 */
+    specification_code         VARCHAR(64)   NOT NULL,                /* 规格编码 */
+    specification_name         VARCHAR(128),                          /* 规格名称 */
+    st_inventory_qty           INT           NOT NULL DEFAULT 0,      /* 起始库存数量 */
+    st_inventory_total         FLOAT(10,2)   NOT NULL DEFAULT '0.00', /* 起始库存总额 */
+    purchase_qty               INT           NOT NULL DEFAULT 0,      /* 采购数量 */
+    purchase_total             FLOAT(10,2)   NOT NULL DEFAULT '0.00', /* 采购总额 */
+    purchase_then_return_qty   INT           NOT NULL DEFAULT 0,      /* 采购退货数量 */
+    purchase_then_return_total FLOAT(10,2)   NOT NULL DEFAULT '0.00', /* 采购退货总额 */
+    sale_qty                   INT           NOT NULL DEFAULT 0,      /* 销售数量 */
+    sale_total                 FLOAT(10,2)   NOT NULL DEFAULT '0.00', /* 销售总额 */
+    sale_then_return_qty       INT           NOT NULL DEFAULT 0,      /* 销售退货数量 */
+    sale_then_return_total     FLOAT(10,2)   NOT NULL DEFAULT '0.00', /* 销售退货总额 */
+    others_qty                 INT           NOT NULL DEFAULT 0,      /* 其他变更数量 */
+    others_total               FLOAT(10,2)   NOT NULL DEFAULT '0.00', /* 其他变更总额 */
+    ed_inventory_qty           INT           NOT NULL DEFAULT 0,      /* 截止库存数量 */
+    ed_inventory_total         FLOAT(10,2)   NOT NULL DEFAULT '0.00', /* 截止库存总额 */
+    create_time                VARCHAR(10),                           /* 年月的格式 */
+    extra_brand                VARCHAR(64),                           /* 品牌 */
+    extra_classification_1     VARCHAR(64),                           /* 分类1 */
+    extra_classification_2     VARCHAR(64),                           /* 分类2 */
+    extra_is_combined          VARCHAR(32),                           /* 是否是组合商品 */
+    anchor                     TINYINT       NOT NULL DEFAULT 0,      /* 锚，防止‘组合商品‘读出来带空格 */
     PRIMARY KEY (id),
     KEY inventories_ct (create_time),
     KEY inventories_specification_code_ct (specification_code, create_time),
